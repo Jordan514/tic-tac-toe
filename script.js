e3c5b4605aa766f2569a0d9b265f13117a517289
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-23 23:53:10 -0500

    Add win conditions, resetGameboard, and updateScores

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let GameBoard = {
   currentPlayer: "",
 };
 
+//Player 2 is always X
 //Factory Function
-function createPlayer(playerName = "default name", playerPosition) {
+function createPlayer(playerName, playerPosition) {
   let score = 0;
   let mark = "";
-  let name = playerName;
+  let name = otherTools.capitalizeFirstLetter(playerName);
   let position = playerPosition;
-  if (playerPosition % 2 === 0) {
+  if (playerPosition === 2) {
     mark = "X";
   } else {
     mark = "O";
@@ -21,6 +22,7 @@ function createPlayer(playerName = "default name", playerPosition) {
 let gameFlow = (function () {
   let pickFirstTurn = () => {
     let player = "player" + Math.floor(Math.random() * 2 + 1);
+    alert(`${players[player]["name"]} goes first!`);
     return player;
   };
   let createPlayers = () => {
@@ -47,18 +49,58 @@ let gameFlow = (function () {
     }
   };
   let checkIfEndGame = (array = []) => {
-    console.log(array[0] + array[1] + array[2]);
-    console.log(array[3] + array[4] + array[5]);
-    console.log(array[6] + array[7] + array[8]);
-
-    console.log(array[0] + array[3] + array[6]);
-    console.log(array[1] + array[4] + array[7]);
-    console.log(array[2] + array[5] + array[8]);
-
-    console.log(array[0] + array[4] + array[8]);
-    console.log(array[6] + array[4] + array[2]);
+    let x = "XXX";
+    let o = "OOO";
+    //row
+    let winr1 = array[0] + array[1] + array[2];
+    let winr2 = array[3] + array[4] + array[5];
+    let winr3 = array[6] + array[7] + array[8];
+    //coloum
+    let winc1 = array[0] + array[3] + array[6];
+    let winc2 = array[1] + array[4] + array[7];
+    let winc3 = array[2] + array[5] + array[8];
+    //diagnal
+    let wind1 = array[0] + array[4] + array[8];
+    let wind2 = array[6] + array[4] + array[2];
+    let winConditionArray = [
+      winr1,
+      winr2,
+      winr3,
+      winc1,
+      winc2,
+      winc3,
+      wind1,
+      wind2,
+    ];
+    for (item of winConditionArray) {
+      if (item == x) {
+        alert(`${players.player2.name} Wins!`);
+        players.player2.score += 1;
+        gameFlow.resetGameboard();
+      } else if (item == o) {
+        alert(`${players.player1.name} Wins!`);
+        players.player1.score += 1;
+        gameFlow.resetGameboard();
+      }
+    }
+    domManipulation.updateScores();
   };
-  return { pickFirstTurn, createPlayers, changePlayer, checkIfEndGame };
+  let resetGameboard = () => {
+    GameBoard.gameBoard = ["", "", "", "", "", "", "", "", ""];
+    // not needed anymore?
+    // for (let i = 0; i < GameBoard.gameBoard.length; i++) {
+    //   document.querySelector("#game-board").removeChild(`.spot${i + 1}`);
+    // }
+    document.querySelector("#game-board").innerHTML = "";
+    domManipulation.createGrid(GameBoard.gameBoard);
+  };
+  return {
+    pickFirstTurn,
+    createPlayers,
+    changePlayer,
+    checkIfEndGame,
+    resetGameboard,
+  };
 })();
 
 let domManipulation = (function () {
@@ -89,11 +131,22 @@ let domManipulation = (function () {
     let gameBoard = document.querySelector("#game-board");
     gameBoard.querySelector(`.spot${markerPosition}`).textContent = mark;
   };
-  return { createGrid, changeNames, addMarker };
+  let updateScores = () => {
+    document.querySelector(".score1").textContent = players.player1.score;
+    document.querySelector(".score2").textContent = players.player2.score;
+  };
+  return { createGrid, changeNames, addMarker, updateScores };
 })();
 
-let players = gameFlow.createPlayers();
+let otherTools = (function () {
+  let capitalizeFirstLetter = (str) => {
+    return str[0].toUpperCase() + str.substring(1, str.length);
+  };
+  return { capitalizeFirstLetter };
+})();
 
+//Setup
+let players = gameFlow.createPlayers();
 //actually playing the game
 GameBoard.currentPlayer = players[gameFlow.pickFirstTurn()];
 domManipulation.createGrid(GameBoard.gameBoard);
